5a18b96bd57e9b8d62c669910285ab356dddfbf1
Matt Kilcup <nevermindthelabel@gmail.com>
2019-08-26 21:39:03 -0700

    switching to typescript

--- routes/api/medications.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const medicationController = require("../../controllers/medicationController");
-
-//Matches with "/api/medications"
-router.route("/")
-	.post(medicationController.create);
-
-module.exports = router;
