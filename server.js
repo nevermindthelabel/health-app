33351856bf02e488d2722ee9f44f6611bb7f8c17
Matt Kilcup <nevermindthelabel@gmail.com>
2019-10-17 22:24:13 -0700

    added cors middleware

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+const cors = require('cors');
 const mongoose = require('mongoose');
 const db = process.env.mongoURI;
 const routes = require('./routes');
+const path = require('path');
 
 const app = express();
 
@@ -12,13 +14,23 @@ const connectDB = () => {
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('DB connected'))
     .catch(error => console.error(error.message));
-    mongoose.set('useCreateIndex', true);
+  mongoose.set('useCreateIndex', true);
 };
 
 connectDB();
 
 app.use(express.json({ extended: false }));
 
+app.use(cors());
+
 app.use(routes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server 👂 on port http://localhost:${PORT}`));
