63eae8e6adc292111a34b9ee0f2ef19f6a67899d
Matt Kilcup <nevermindthelabel@gmail.com>
2019-10-19 22:08:22 -0700

    added Dashboard page

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Dashboard = () => {
+  return(
+    <div className='container'>
+      You have reached the Dashboard.
+    </div>
+  )
+}
+
+export default Dashboard;
